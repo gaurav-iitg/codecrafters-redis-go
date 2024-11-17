0636ddcd8ee51b8771dfd22d1317b55c54363d9c
gaurav-iitg <gauravkumar5089@gmail.com>
2024-11-17 13:15:30 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -13,16 +13,16 @@ const (
 
 func main() {
 
-	l, err := net.Listen("tcp", address)
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("Failed to listen on %s: %v", address, err)
 		os.Exit(1)
 	}
-	defer l.Close()
+	defer listener.Close()
 	log.Printf("Listening on %s", address)
 
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("Error accepting connection: %v", err)
 			continue
@@ -35,18 +35,20 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	log.Printf("Accepted connection from %s", conn.RemoteAddr())
 
-	buf := make([]byte, bufferSize)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Printf("Error reading from connection: %v", err)
-		return
-	}
+	for {
+		buf := make([]byte, bufferSize)
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Printf("Error reading from connection: %v", err)
+			return
+		}
+ 
+		log.Printf("Read command: %s", buf[:n])
 
-	log.Printf("Read command: %s", buf[:n])
-
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		log.Printf("Error writing to connection: %v", err)
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			log.Printf("Error writing to connection: %v", err)
+		}
 	}
 
 }
