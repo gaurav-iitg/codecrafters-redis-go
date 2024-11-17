773437e13cc047ef72c95da1bcb4d30c684cead1
gaurav-iitg <gauravkumar5089@gmail.com>
2024-11-17 12:02:59 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,29 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+const (
+	address    = "0.0.0.0:6379"
+	bufferSize = 128
+)
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", address)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		log.Fatalf("Failed to listen on %s: %v", address, err)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	log.Printf("Listening on %s", address)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	log.Printf("Accepted connection from %s", conn.RemoteAddr())
+
+	buf := make([]byte, bufferSize)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Printf("Error reading from connection: %v", err)
+		return
+	}
+
+	log.Printf("Read command: %s", buf[:n])
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		log.Printf("Error writing to connection: %v", err)
+	}
+
 }
